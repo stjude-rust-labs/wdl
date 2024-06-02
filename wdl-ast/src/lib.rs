10a3b63e27ed85042d0c7ba1582f9218b06cd2d2
Peter Huene <peter@huene.dev>
2024-06-01 22:36:02 -0700

    fix: remove unused nightly features from crates.
    This commit removes unused nightly features from crates, getting the `wdl`
    family of crates closer to building on stable.
    
    The remaining holdout is `wdl-macros`, which will soon be removed entirely.

--- wdl-ast/src/lib.rs
+++ wdl-ast/src/lib.rs
@@ -1,6 +1,5 @@
 //! An abstract syntax tree for Workflow Description Language documents.
 
-#![feature(decl_macro)]
 #![warn(missing_docs)]
 #![warn(rust_2018_idioms)]
 #![warn(rust_2021_compatibility)]
