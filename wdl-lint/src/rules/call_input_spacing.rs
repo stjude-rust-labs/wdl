e777630ba6d4da0e807688685588a752cc6d96ec
Andrew Thrasher <adthrasher@gmail.com>
2024-06-28 15:07:47 -0400

    chore: apply feedback from PR

--- wdl-lint/src/rules/call_input_spacing.rs
+++ wdl-lint/src/rules/call_input_spacing.rs
@@ -1,5 +1,7 @@
 //! A lint rule for spacing of call inputs.
 
+use std::borrow::Borrow;
+
 use wdl_ast::v1::CallStatement;
 use wdl_ast::AstNode;
 use wdl_ast::Diagnostic;
@@ -18,24 +20,13 @@ use crate::TagSet;
 const ID: &str = "CallInputSpacing";
 
 /// Creates a input spacing diagnostic.
-fn call_input_spacing(span: Span) -> Diagnostic {
-    Diagnostic::warning("input not properly spaced")
+fn call_input_keyword_spacing(span: Span) -> Diagnostic {
+    Diagnostic::warning("call input keyword not properly spaced")
         .with_rule(ID)
         .with_highlight(span)
         .with_fix(
             "input keyword must be separated from the opening brace (\"{\") by a single space"
                 .to_string(),
-        )
-}
-
-/// Creates an input keyword preceding newline diagnostic.
-fn call_input_keyword_preceding_newline(span: Span) -> Diagnostic {
-    Diagnostic::warning("input keyword may not be preceded by a newline")
-        .with_rule(ID)
-        .with_highlight(span)
-        .with_fix(
-            "input keyword must be separated from the opening brace (\"{\") by a single space \
-             with no newlines",
         )
 }
 
@@ -102,82 +93,82 @@ impl Visitor for CallInputSpacingRule {
             return;
         }
 
-        let mut input_seen = false;
-        let mut newline_seen = 0;
-        call.syntax().children_with_tokens().for_each(|c| {
-            match c.kind() {
-                SyntaxKind::OpenBrace => {
-                    let next = c.next_sibling_or_token().unwrap();
-                    match next.kind() {
-                        // The opening brace of a call block must be followed by exactly one space,
-                        // the input keyword, a colon and a newline
-                        SyntaxKind::Whitespace => {
-                            if next.to_string() != " " {
-                                // Check for a single space
-                                state.add(call_input_spacing(c.text_range().to_span()));
-                            } else {
-                                // Check for the input keyword
-                                match next.next_sibling_or_token().unwrap().kind() {
-                                    SyntaxKind::InputKeyword => {}
-                                    _ => {
-                                        state.add(call_input_spacing(c.text_range().to_span()));
-                                    }
+        // Check for "{ input:" spacing
+        let mut spacing_errors = 0;
+        let input_keyword = call
+            .syntax()
+            .children_with_tokens()
+            .find(|c| c.kind() == SyntaxKind::InputKeyword);
+
+        if let Some(rbrace) = call
+            .syntax()
+            .children_with_tokens()
+            .find(|c| c.kind() == SyntaxKind::OpenBrace)
+        {
+            match input_keyword.borrow() {
+                Some(t) => {
+                    if let Some(next) = rbrace.next_sibling_or_token() {
+                        if next.as_token().expect("should be a token").text().eq(" ") {
+                            if let Some(second) = next.next_sibling_or_token() {
+                                if second != *t {
+                                    spacing_errors += 1;
                                 }
                             }
-                        }
-                        _ => {
-                            // Opening brace is followed by something other than whitespace
-                            state.add(call_input_spacing(c.text_range().to_span()));
-                        }
-                    }
-                }
-                SyntaxKind::InputKeyword => {
-                    input_seen = true;
-                    newline_seen = 0;
-                }
-                SyntaxKind::Whitespace => {
-                    if c.to_string().contains('\n') {
-                        if !input_seen {
-                            // Newlines are not allowed before the input keyword
-                            state.add(call_input_keyword_preceding_newline(
-                                c.text_range().to_span(),
-                            ));
                         } else {
-                            newline_seen += 1;
+                            spacing_errors += 1;
                         }
                     }
                 }
-                SyntaxKind::CallInputItemNode => {
-                    if newline_seen > 0 {
-                        // Reset newlines seen, since this is an input
-                        // Empty lines will be detected by the Whitespace rule
-                        newline_seen = 0;
-                    } else if inputs > 1 {
-                        // Only check for newlines if there are multiple inputs
-                        state.add(call_input_missing_newline(c.text_range().to_span()));
-                    }
+                None => {
+                    // input keyword will be optional in the future
                 }
-                _ => {}
             }
-        });
+            if spacing_errors > 0 {
+                state.add(call_input_keyword_spacing(Span::new(
+                    rbrace.text_range().to_span().start(),
+                    input_keyword.unwrap().text_range().to_span().start()
+                        - rbrace.text_range().to_span().end()
+                        + 1,
+                )));
+            }
+        }
 
-        // Check for assignment spacing
         call.inputs().for_each(|input| {
-            input
+            // Check for assignment spacing
+            if let Some(assign) = input
                 .syntax()
                 .children_with_tokens()
                 .find(|c| c.kind() == SyntaxKind::Assignment)
-                .map(|c| {
-                    match (
-                        c.next_sibling_or_token().unwrap().kind(),
-                        c.prev_sibling_or_token().unwrap().kind(),
-                    ) {
-                        (SyntaxKind::Whitespace, SyntaxKind::Whitespace) => {}
-                        _ => {
-                            state.add(call_input_assignment(c.text_range().to_span()));
-                        }
+            {
+                match (
+                    assign.next_sibling_or_token().unwrap().kind(),
+                    assign.prev_sibling_or_token().unwrap().kind(),
+                ) {
+                    (SyntaxKind::Whitespace, SyntaxKind::Whitespace) => {}
+                    _ => {
+                        state.add(call_input_assignment(assign.text_range().to_span()));
                     }
-                });
+                }
+            }
         });
+
+        // Check for one input per line
+        let mut newline_seen = 0;
+        call.syntax()
+            .children_with_tokens()
+            .for_each(|c| match c.kind() {
+                SyntaxKind::Whitespace => {
+                    if c.to_string().contains('\n') {
+                        newline_seen += 1;
+                    }
+                }
+                SyntaxKind::CallInputItemNode => {
+                    if newline_seen == 0 && inputs > 1 {
+                        state.add(call_input_missing_newline(c.text_range().to_span()));
+                    }
+                    newline_seen = 0;
+                }
+                _ => {}
+            });
     }
 }
