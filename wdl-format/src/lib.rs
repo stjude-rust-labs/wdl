a961355a7307844aee96493fd2edb06f965b6397
Andrew Frantz <andrew.frantz@stjude.org>
2024-07-03 13:10:24 -0400

    [WIP]feat: new wdl-format crate

---
+++ wdl-format/src/lib.rs
@@ -0,0 +1,10 @@
+//! A library for auto-formatting WDL code.
+
+#![warn(missing_docs)]
+#![warn(rust_2018_idioms)]
+#![warn(rust_2021_compatibility)]
+#![warn(missing_debug_implementations)]
+#![warn(clippy::missing_docs_in_private_items)]
+#![warn(rustdoc::broken_intra_doc_links)]
+
+pub mod format;
