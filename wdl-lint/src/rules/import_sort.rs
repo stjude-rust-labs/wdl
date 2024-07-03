1fc367eed346f1d5d21f6842df4cd6ddb635b446
Andrew Frantz <andrew.frantz@stjude.org>
2024-07-03 19:03:34 -0400

    Update import_sort.rs

--- wdl-lint/src/rules/import_sort.rs
+++ wdl-lint/src/rules/import_sort.rs
@@ -8,7 +8,6 @@ use wdl_ast::Document;
 use wdl_ast::Span;
 use wdl_ast::SupportedVersion;
 use wdl_ast::SyntaxKind;
-use wdl_ast::SyntaxNode;
 use wdl_ast::ToSpan;
 use wdl_ast::VisitReason;
 use wdl_ast::Visitor;
