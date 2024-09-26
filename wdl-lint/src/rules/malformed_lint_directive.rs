4886743d499fd1392d69e1ba99420ed5f06e82f6
Michael Gattas <michael.gattas@stjude.org>
2024-09-26 14:38:53 -0500

    In-Progress: Malformed Lint Directive

--- wdl-lint/src/rules/malformed_lint_directive.rs
+++ wdl-lint/src/rules/malformed_lint_directive.rs
@@ -1,6 +1,10 @@
 //! A lint rule for flagging malformed lint directives.
 
+use std::collections::HashMap;
+use std::sync::LazyLock;
+use wdl_ast::AstToken;
 use wdl_ast::Comment;
+use wdl_ast::EXCEPT_COMMENT_PREFIX;
 use wdl_ast::Diagnostic;
 use wdl_ast::Diagnostics;
 use wdl_ast::Document;
@@ -11,7 +15,9 @@ use wdl_ast::SyntaxKind;
 use wdl_ast::ToSpan;
 use wdl_ast::VisitReason;
 use wdl_ast::Visitor;
+use crate::rules;
 use crate::Rule;
+use crate::rules::RULE_MAP;
 use crate::Tag;
 use crate::TagSet;
 
@@ -31,16 +37,16 @@ fn malformed_lint_directive(
         .collect::<Vec<_>>()
         .join(", ");
 
-    Diagnostic::error(format!(
+    Diagnostic::warning(format!(
         "lint directive `{id}` is malformed"
     ))
         .with_label("cannot make an exception for this rule", span)
         .with_label(
-            "invalid element for this lint directive",
+            "malformed lint directive",
             wrong_element.text_range().to_span(),
         )
         .with_fix(format!(
-            "valid locations for this directive are above: {locations}"
+            "format your lint direct with as #@ except: {locations}"
         ))
 }
 
@@ -58,7 +64,8 @@ impl Rule for MalformedLintDirectiveRule {
     }
 
     fn explanation(&self) -> &'static str {
-        todo!()
+        "Comments which begin with `#@` must only contain valid lint directives. Lint directives \
+        must be on their own line, only preceded by whitespace."
     }
 
 
@@ -67,7 +74,8 @@ impl Rule for MalformedLintDirectiveRule {
     }
 
     fn exceptable_nodes(&self) -> Option<&'static [wdl_ast::SyntaxKind]> {
-        Some(&[SyntaxKind::VersionStatementNode])
+        // Some(&[SyntaxKind::VersionStatementNode])
+        None
     }
 }
 
@@ -78,50 +86,54 @@ impl Visitor for MalformedLintDirectiveRule {
         // This is intentionally empty, as this rule has no state.
     }
 
-    fn comment(&mut self, _state: &mut Self::State, comment: &Comment) {
-        println!("Comment: {:?}", comment);
-        // if let Some(ids) = comment.as_str().strip_prefix(EXCEPT_COMMENT_PREFIX) {
-        //     let start: usize = comment.span().start();
-        //     let mut offset = EXCEPT_COMMENT_PREFIX.len();
-        //
-        //     let excepted_element = comment
-        //         .syntax()
-        //         .siblings_with_tokens(rowan::Direction::Next)
-        //         .find_map(|s| {
-        //             if s.kind() == SyntaxKind::Whitespace || s.kind() == SyntaxKind::Comment {
-        //                 None
-        //             } else {
-        //                 Some(s)
-        //             }
-        //         });
-        //
-        //     for id in ids.split(',') {
-        //         // First trim the start so we can determine how much whitespace was removed
-        //         let trimmed_start = id.trim_start();
-        //         // Next trim the end
-        //         let trimmed: &str = trimmed_start.trim_end();
-        //
-        //         // Update the offset to account for the whitespace that was removed
-        //         offset += id.len() - trimmed.len();
-        //
-        //         if let Some(elem) = &excepted_element {
-        //             if let Some(Some(exceptable_nodes)) = RULE_MAP.get(trimmed) {
-        //                 if !exceptable_nodes.contains(&elem.kind()) {
-        //                     state.add(crate::rules::misplaced_lint_directive::misplaced_lint_directive(
-        //                         trimmed,
-        //                         Span::new(start + offset, trimmed.len()),
-        //                         elem,
-        //                         exceptable_nodes,
-        //                     ));
-        //                 }
-        //             }
-        //         }
-        //
-        //         // Update the offset to account for the rule id and comma
-        //         offset += trimmed.len() + 1;
-        //     }
-        // }
+    fn comment(&mut self, state: &mut Self::State, comment: &Comment) {
+        // println!("Comment: {:?}", comment);
+        if let x = comment.syntax().prev_token() {
+            let y = x.unwrap().kind();
+            println!("{:?}", y);
+        }
     }
-
-
+    //     if let Some(ids) = comment.as_str().strip_prefix(EXCEPT_COMMENT_PREFIX) {
+    //         let start: usize = comment.span().start();
+    //         let mut offset = EXCEPT_COMMENT_PREFIX.len();
+    //         println!("ids: {:?}", ids);
+    //
+    //         let excepted_element = comment
+    //             .syntax()
+    //             .siblings_with_tokens(rowan::Direction::Next)
+    //             .find_map(|s| {
+    //                 if s.kind() == SyntaxKind::Whitespace || s.kind() == SyntaxKind::Comment {
+    //                     None
+    //                 } else {
+    //                     Some(s)
+    //                 }
+    //             });
+    //
+    //         for id in ids.split(',') {
+    //             // First trim the start so we can determine how much whitespace was removed
+    //             let trimmed_start = id.trim_start();
+    //             // Next trim the end
+    //             let trimmed: &str = trimmed_start.trim_end();
+    //
+    //             // Update the offset to account for the whitespace that was removed
+    //             offset += id.len() - trimmed.len();
+    //
+    //             if let Some(elem) = &excepted_element {
+    //                 if let Some(Some(exceptable_nodes)) = RULE_MAP.get(trimmed) {
+    //                     if !exceptable_nodes.contains(&elem.kind()) {
+    //                         state.add(malformed_lint_directive(
+    //                             trimmed,
+    //                             Span::new(start + offset, trimmed.len()),
+    //                             elem,
+    //                             exceptable_nodes,
+    //                         ));
+    //                     }
+    //                 }
+    //             }
+    //
+    //             // Update the offset to account for the rule id and comma
+    //             offset += trimmed.len() + 1;
+    //         }
+    //     }
+    // }
 }
