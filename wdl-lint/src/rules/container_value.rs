dcea87cccf2d702bc69a474a4fd588fdb152c68f
Andrew Frantz <andrew.frantz@stjude.org>
2024-12-05 15:21:35 -0500

    chore: review feedback

--- wdl-lint/src/rules/container_value.rs
+++ wdl-lint/src/rules/container_value.rs
@@ -78,12 +78,14 @@ fn array_to_string_literal(span: Span) -> Diagnostic {
 /// Creates a diagnostic indicating that an array contains one or more 'any'
 /// URIs.
 fn array_containing_anys(spans: impl Iterator<Item = Span>) -> Diagnostic {
-    let mut diagnostic = Diagnostic::warning(format!("container arrays containing `{ANY_CONTAINER_VALUE}` are ambiguous"))
-        .with_rule(ID)
-        .with_fix(format!(
-            "remove these entries or change the array to a string literal with the value of \
-             `{ANY_CONTAINER_VALUE}`"
-        ));
+    let mut diagnostic = Diagnostic::warning(format!(
+        "container arrays containing `{ANY_CONTAINER_VALUE}` are ambiguous"
+    ))
+    .with_rule(ID)
+    .with_fix(format!(
+        "remove these entries or change the array to a string literal with the value of \
+         `{ANY_CONTAINER_VALUE}`"
+    ));
 
     for span in spans {
         diagnostic = diagnostic.with_highlight(span)
