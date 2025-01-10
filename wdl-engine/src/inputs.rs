stjude-rust-labs/wdl

commit 4f1cf71802933415a6d5afcd84b08b5df812c6c7

fix: fix validation not to treat specified call inputs as missing. (#282)

wdl-engine/src/inputs.rs
@@ -1,6 +1,7 @@
 //! Implementation of workflow and task inputs.
 
 use std::collections::HashMap;
+use std::collections::HashSet;
 use std::fs::File;
 use std::io::BufReader;
 use std::mem;
@@ -111,8 +112,14 @@ impl TaskInputs {
 
     /// Validates the inputs for the given task.
     ///
-    /// Note that this alters the inputs
-    pub fn validate(&self, document: &Document, task: &Task) -> Result<()> {
+    /// The `specified` set of inputs are those that are present, but may not
+    /// have values available at validation.
+    pub fn validate(
+        &self,
+        document: &Document,
+        task: &Task,
+        specified: Option<&HashSet<String>>,
+    ) -> Result<()> {
         let version = document.version().context("missing document version")?;
 
         // Start by validating all the specified inputs and their types
@@ -132,8 +139,14 @@ impl TaskInputs {
 
         // Next check for missing required inputs
         for (name, input) in task.inputs() {
-            if input.required() && !self.inputs.contains_key(name) {
-                bail!("missing required input `{name}`");
+            if input.required()
+                && !self.inputs.contains_key(name)
+                && specified.map(|s| !s.contains(name)).unwrap_or(true)
+            {
+                bail!(
+                    "missing required input `{name}` to task `{task}`",
+                    task = task.name()
+                );
             }
         }
 
@@ -319,7 +332,15 @@ impl WorkflowInputs {
     }
 
     /// Validates the inputs for the given workflow.
-    pub fn validate(&self, document: &Document, workflow: &Workflow) -> Result<()> {
+    ///
+    /// The `specified` set of inputs are those that are present, but may not
+    /// have values available at validation.
+    pub fn validate(
+        &self,
+        document: &Document,
+        workflow: &Workflow,
+        specified: Option<&HashSet<String>>,
+    ) -> Result<()> {
         // Start by validating all the specified inputs and their types
         for (name, value) in &self.inputs {
             let input = workflow
@@ -335,8 +356,14 @@ impl WorkflowInputs {
 
         // Next check for missing required inputs
         for (name, input) in workflow.inputs() {
-            if input.required() && !self.inputs.contains_key(name) {
-                bail!("missing required input `{name}`");
+            if input.required()
+                && !self.inputs.contains_key(name)
+                && specified.map(|s| !s.contains(name)).unwrap_or(false)
+            {
+                bail!(
+                    "missing required input `{name}` to workflow `{workflow}`",
+                    workflow = workflow.name()
+                );
             }
         }
 
@@ -379,7 +406,7 @@ impl WorkflowInputs {
                         format!("`{name}` is a call to a task, but workflow inputs were supplied")
                     })?;
 
-                    task_inputs.validate(document, task)?;
+                    task_inputs.validate(document, task, Some(call.specified()))?;
                     &task_inputs.inputs
                 }
                 CallKind::Workflow => {
@@ -393,7 +420,7 @@ impl WorkflowInputs {
                         format!("`{name}` is a call to a workflow, but task inputs were supplied")
                     })?;
 
-                    workflow_inputs.validate(document, workflow)?;
+                    workflow_inputs.validate(document, workflow, Some(call.specified()))?;
                     &workflow_inputs.inputs
                 }
             };

wdl-engine/tests/inputs.rs
@@ -156,6 +156,7 @@ fn run_test(test: &Path, result: AnalysisResult, ntests: &AtomicUsize) -> Result
                         document
                             .task_by_name(&name)
                             .expect("task should be present"),
+                        None,
                     )
                     .with_context(|| format!("failed to validate the inputs to task `{name}`"))
                 {
@@ -165,7 +166,7 @@ fn run_test(test: &Path, result: AnalysisResult, ntests: &AtomicUsize) -> Result
             }
             Inputs::Workflow(inputs) => {
                 let workflow = document.workflow().expect("workflow should be present");
-                match inputs.validate(document, workflow).with_context(|| {
+                match inputs.validate(document, workflow, None).with_context(|| {
                     format!(
                         "failed to validate the inputs to workflow `{workflow}`",
                         workflow = workflow.name()

wdl-engine/src/eval/v1/task.rs
@@ -206,7 +206,7 @@ impl<'a> TaskEvaluator<'a> {
             return Err(diagnostic.clone().into());
         }
 
-        inputs.validate(document, task).with_context(|| {
+        inputs.validate(document, task, None).with_context(|| {
             format!(
                 "failed to validate the inputs to task `{task}`",
                 task = task.name()

wdl-engine/tests/inputs/call-input-present/source.wdl
@@ -1,0 +1,23 @@
+## This is a test of a call input being present.
+## No error should be present.
+
+version 1.2
+
+task foo {
+    input {
+        String a
+        String b
+    }
+
+    command <<<>>>
+}
+
+workflow bar {
+    hints {
+        allow_nested_inputs: true
+    }
+
+    call foo {
+        a = "Hello"
+    }
+}

commit d408df77919f88acd25d5605bfc6aaffb6b11392

fix: fix a regression caused by #282.
This fixes a regression in validating workflow inputs caused by an incorrect
`false` when `true` should have been used; the regression was introduced in
PR #282.

wdl-engine/src/inputs.rs
@@ -358,7 +358,7 @@ impl WorkflowInputs {
         for (name, input) in workflow.inputs() {
             if input.required()
                 && !self.inputs.contains_key(name)
-                && specified.map(|s| !s.contains(name)).unwrap_or(false)
+                && specified.map(|s| !s.contains(name)).unwrap_or(true)
             {
                 bail!(
                     "missing required input `{name}` to workflow `{workflow}`",