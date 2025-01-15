1c2b6c77014964628c7105a19ce2cb9306474cec
Andrew Frantz <andrew.frantz@stjude.org>
2025-01-15 13:50:04 -0500

    chore: review feedback

--- wdl-format/src/config/builder.rs
+++ wdl-format/src/config/builder.rs
@@ -4,36 +4,6 @@ use crate::Config;
 use crate::config::Indent;
 use crate::config::MaxLineLength;
 
-/// An error related to a [`Builder`].
-#[derive(Debug)]
-pub enum Error {
-    /// A required value was missing for a builder field.
-    Missing(&'static str),
-
-    /// An invalid value was provided for a builder field.
-    Invalid(&'static str),
-}
-
-impl std::fmt::Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Error::Missing(field) => write!(
-                f,
-                "missing required value for '{field}' in a formatter configuration builder"
-            ),
-            Error::Invalid(field) => write!(
-                f,
-                "invalid value for '{field}' in a formatter configuration builder"
-            ),
-        }
-    }
-}
-
-impl std::error::Error for Error {}
-
-/// A [`Result`](std::result::Result) with an [`Error`].
-pub type Result<T> = std::result::Result<T, Error>;
-
 /// A builder for a [`Config`].
 #[derive(Default)]
 pub struct Builder {
--- wdl-format/src/config/max_line_length.rs
+++ wdl-format/src/config/max_line_length.rs
@@ -1,22 +1,5 @@
 //! Configuration for max line length formatting.
 
-/// The error type for max line length configuration.
-pub enum Error {
-    /// The max line length is invalid.
-    Invalid(&'static str),
-}
-
-impl std::fmt::Display for Error {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Error::Invalid(field) => write!(
-                f,
-                "invalid value for '{field}' in a formatter configuration builder"
-            ),
-        }
-    }
-}
-
 /// The default maximum line length.
 pub const DEFAULT_MAX_LINE_LENGTH: usize = 90;
 /// The minimum maximum line length.
@@ -32,11 +15,16 @@ impl MaxLineLength {
     /// Attempts to create a new `MaxLineLength` with the provided value.
     ///
     /// A value of `0` indicates no maximum.
-    pub fn try_new(value: usize) -> Result<Self, Error> {
+    pub fn try_new(value: usize) -> Result<Self, String> {
         let val = match value {
             0 => Self(None),
             MIN_MAX_LINE_LENGTH..=MAX_MAX_LINE_LENGTH => Self(Some(value)),
-            _ => return Err(Error::Invalid("max_line_length")),
+            _ => {
+                return Err(format!(
+                    "The maximum line length must be between {} and {} or 0",
+                    MIN_MAX_LINE_LENGTH, MAX_MAX_LINE_LENGTH
+                ));
+            }
         };
         Ok(val)
     }
