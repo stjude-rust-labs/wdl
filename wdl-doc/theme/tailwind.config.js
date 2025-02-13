cb9ecbede69936da8ce8c3043edb78f7dbd111da
Andrew Frantz <andrew.frantz@stjude.org>
2025-02-13 07:47:36 -0500

    Delete tailwind.config.js

--- wdl-doc/theme/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  mode: "all",
-  content: ["./src/**/*.css", "../src/**/*.rs"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
