a88eb18f178c1f45f5603d06925477e651545c45
Ankush Chudiwal <154556772+Ankush1oo8@users.noreply.github.com>
2025-03-10 21:17:03 +0530

    Update v1.rs

--- wdl-analysis/src/types/v1.rs
+++ wdl-analysis/src/types/v1.rs
@@ -696,10 +696,8 @@ impl<'a, C: EvaluationContext> ExprTypeEvaluator<'a, C> {
                 }
             }
         }
-    
         self.placeholders -= 1;
     }    
-
     /// Evaluates the type of a literal array expression.
     fn evaluate_literal_array<N: TreeNode>(&mut self, expr: &LiteralArray<N>) -> Type {
         // Look at the first array element to determine the element type
