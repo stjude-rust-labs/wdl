ef3570c972baa636d694afda5b5c1dc154566382
rajiknows <rajeshhjhamain@gmail.com>
2025-05-28 19:22:05 +0530

    review needed

--- wdl-lint/src/rules/object_to_struct.rs
+++ wdl-lint/src/rules/object_to_struct.rs
@@ -1,13 +1,12 @@
 use wdl_analysis::Diagnostics;
 use wdl_analysis::VisitReason;
 use wdl_analysis::Visitor;
-use wdl_analysis::types::Type;
 use wdl_ast::AstNode;
 use wdl_ast::Diagnostic;
 use wdl_ast::Span;
 use wdl_ast::SyntaxElement;
 use wdl_ast::SyntaxKind;
-use wdl_ast::v1::BoundDecl;
+use wdl_ast::v1::Expr::Literal;
 use wdl_ast::v1::Type;
 
 use crate::Rule;
@@ -23,7 +22,8 @@ fn prefer_struct_over_object(span: Span) -> Diagnostic {
         .with_fix("Define a specific `Struct` type and use it instead of `Object`.")
 }
 
-#[derive(Default, Debug, Clone, Copy)]
+// A rule that discourages object to struct coercion
+#[derive(Default, Debug)]
 pub struct ObjectToStructRule;
 
 impl Rule for ObjectToStructRule {
@@ -32,31 +32,21 @@ impl Rule for ObjectToStructRule {
     }
 
     fn description(&self) -> &'static str {
-        "Discourages direct use of `Object` type, recommending `Struct` definitions for better \
-         type safety and clarity."
+        "Discourages object to struct coercion"
     }
 
     fn explanation(&self) -> &'static str {
-        "WDL supports object-to-struct coercion, allowing `Object` literals to be assigned to \
-         `Struct`-typed variables. However, declarations should use specific `Struct` types \
-         directly whenever the data structure is fixed and known. Using explicit `Struct`s \
-         enhances type safety, improves code readability, and provides more precise information \
-         for downstream analysis tools. Declaring variables as `Object` when a `Struct` could be \
-         defined can obscure the intended data schema."
+        "WDL allows assigning `Object` literals to `Struct`-typed variables, but this hides the \
+         intended schema. Prefer defining explicit `Struct` types to improve type safety, clarity, \
+         and tool support."
     }
 
     fn tags(&self) -> TagSet {
-        TagSet::new(&[Tag::Clarity])
+        TagSet::new(&[Tag::Clarity, Tag::Deprecated])
     }
 
     fn exceptable_nodes(&self) -> Option<&'static [wdl_ast::SyntaxKind]> {
-        Some(&[
-            SyntaxKind::VersionStatementNode,
-            SyntaxKind::TaskDefinitionNode,
-            SyntaxKind::WorkflowDefinitionNode,
-            SyntaxKind::BoundDeclNode,
-            SyntaxKind::UnboundDeclNode,
-        ])
+        Some(&[SyntaxKind::BoundDeclNode])
     }
 
     fn related_rules(&self) -> &[&'static str] {
@@ -69,39 +59,30 @@ impl Visitor for ObjectToStructRule {
         *self = Self::default();
     }
 
-    fn bound_decl(&mut self, diagnostics: &mut Diagnostics, reason: VisitReason, decl: &BoundDecl) {
+    fn bound_decl(
+        &mut self,
+        diagnostics: &mut Diagnostics,
+        reason: VisitReason,
+        decl: &wdl_ast::v1::BoundDecl,
+    ) {
         if reason == VisitReason::Exit {
             return;
         }
 
-        // match Type::as_struct(&self){
-        //    Some(structnode)=>{
-        //        match decl.expr() {
-        //
-        // wdl_ast::v1::Expr::Literal(wdl_ast::v1::LiteralExpr::Object(token)) => {
-        //                diagnostics.exceptable_add(
-        //                    prefer_struct_over_object(token.span()),
-        //                    SyntaxElement::from(decl.inner().clone()),
-        //                    &self.exceptable_nodes(),
-        //                );
-        //            }
-        //
-        //        _ => {}
-        //        }
-        //
-        //    },
-        //    None=>{}
-        //}
-
-        // we check if the expression that is being initialised to the declaration is an
-        // object type or not
-        match decl.expr() {
-            wdl_ast::v1::Expr::Literal(wdl_ast::v1::LiteralExpr::Object(token)) => {
-                diagnostics.exceptable_add(
-                    prefer_struct_over_object(token.span()),
-                    SyntaxElement::from(decl.inner().clone()),
-                    &self.exceptable_nodes(),
-                );
+        match decl.ty() {
+            // if it is a reference to a variable
+            // likely ...
+            // struct myStruct;
+            // myStruct = Object {}
+            Type::Ref(_) => {
+                // we check if it is bounded to a object literal declaration
+                if let Literal(wdl_ast::v1::LiteralExpr::Object(object_type_token)) = decl.expr() {
+                    diagnostics.exceptable_add(
+                        prefer_struct_over_object(object_type_token.span()),
+                        SyntaxElement::from(decl.inner().clone()),
+                        &self.exceptable_nodes(),
+                    )
+                }
             }
             _ => {}
         }
