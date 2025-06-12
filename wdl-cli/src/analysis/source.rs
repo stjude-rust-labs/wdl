0d638069da1bb70a81774349a46c9e9ce389a4d2
Andrew Thrasher <adthrasher@gmail.com>
2025-06-12 13:08:13 -0400

    chore: closing delim

--- wdl-cli/src/analysis/source.rs
+++ wdl-cli/src/analysis/source.rs
@@ -88,7 +88,10 @@ impl std::str::FromStr for Source {
 impl Default for Source {
     fn default() -> Self {
         // Default to the current directory.
-        Source::Directory(std::env::current_dir().unwrap_or_else(|_| PathBuf::from(std::path::Component::CurDir.as_os_str()))
+        Source::Directory(
+            std::env::current_dir()
+                .unwrap_or_else(|_| PathBuf::from(std::path::Component::CurDir.as_os_str())),
+        )
     }
 }
 
