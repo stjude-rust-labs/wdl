f8a475b91df99f6a62adac78fbb3a0a7c5a74733
Vaibhav Raj <vaibhavmonu8309@gmail.com>
2025-07-08 08:42:40 +0530

    feat: add support for completions

--- wdl-analysis/src/handlers.rs
+++ wdl-analysis/src/handlers.rs
@@ -1,7 +1,9 @@
 //! Language server protocol handlers.
 
+mod completions;
 mod find_all_references;
 mod goto_definition;
 
+pub use completions::*;
 pub use find_all_references::*;
 pub use goto_definition::*;
