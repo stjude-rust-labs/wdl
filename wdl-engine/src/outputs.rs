f9a72e72fa81bc6d317257f9d1849fc3d505394d
Andrew Thrasher <adthrasher@gmail.com>
2025-07-23 17:39:47 -0400

    chore: fmt

--- wdl-engine/src/outputs.rs
+++ wdl-engine/src/outputs.rs
@@ -84,9 +84,7 @@ impl Serialize for Outputs {
         for (k, v) in &self.values {
             let v = v.serializable_with_pairs();
             match &self.name {
-                Some(prefix) => {
-                    s.serialize_entry(&format!("{prefix}.{k}"), &v)?
-                }
+                Some(prefix) => s.serialize_entry(&format!("{prefix}.{k}"), &v)?,
                 None => s.serialize_entry(k, &v)?,
             }
         }
