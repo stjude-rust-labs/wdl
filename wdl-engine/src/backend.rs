422fe8e9449cd570333a5a1bd179faf728ae6270
Adam C. Foltzer <adam.foltzer@stjude.org>
2025-09-11 12:06:01 -0700

    wip: build out more glue code for the new backend

--- wdl-engine/src/backend.rs
+++ wdl-engine/src/backend.rs
@@ -336,6 +336,7 @@ impl<Req> TaskManagerState<Req> {
 }
 
 /// Responsible for managing tasks based on available host resources.
+#[derive(Debug)]
 struct TaskManager<Req> {
     /// The sender for new spawn requests.
     tx: mpsc::UnboundedSender<(Req, oneshot::Sender<Result<TaskExecutionResult>>)>,
